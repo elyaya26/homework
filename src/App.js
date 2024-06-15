07c511e23db94b2b786aff900df73c8678643050
elyaya26 <ermatova-elina@mail.ru>
2024-06-15 23:03:49 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [facts, setFacts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://meowfacts.herokuapp.com/?count=3');
+                const data = await response.json();
+                setFacts(data.data);
+                console.log(data.data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>Cat Facts</h1>
+
+            <ul>
+                {facts.map((fact, index) => {
+                    return (
+                        <li key={index}>
+                            {fact}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
